62491ad1092ff6a9c95e57878f6eda172681c0e5
Joe Sak <joe@joesak.com>
2016-02-12 11:04:45 -0500

    Parse emphasis

--- lib/markdown/parser.rb
+++ lib/markdown/parser.rb
@@ -14,17 +14,26 @@ module Markdown
       parsed_lines = []
 
       file.each_line do |line|
-        if header_match = line.chomp.match(/^(#+)\s(.+)$/)
+        line.chomp!
+
+        if header_match = line.match(/^(#+)\s(.+)$/)
           header_level = header_match[1].length
           content = header_match[2]
           parsed_lines.push("<h#{header_level}>#{content}</h#{header_level}>")
+        elsif emphasis_match = line.match(/^(.*)(\*|_)(.+)\2(.*)$/)
+          before_content = emphasis_match[1]
+          content = emphasis_match[3]
+          after_content = emphasis_match[4]
+          parsed_lines.push("#{before_content}<em>#{content}</em>#{after_content}")
+        else
+          parsed_lines.push(line)
         end
       end
 
       parsed_lines.join
     end
 
-    class HeaderParser
+    class HeaderTagParser
     end
   end
 end
--- spec/markdown/parser_spec.rb
+++ spec/markdown/parser_spec.rb
@@ -1,17 +1,39 @@
 require 'spec_helper'
 
 describe Markdown::Parser do
+  def parse_as_file(str)
+    file = StringIO.new("#{str}\n")
+    parser = described_class.new(file)
+    parser.parse
+  end
+
   describe "#parse" do
     (1..6).each do |header_level|
       it "parses header #{header_level}" do
         hashes = '#' * header_level
         content = "Heading #{header_level}"
-        file = StringIO.new("#{hashes} #{content}\n")
 
-        parser = described_class.new(file)
-        parsed = parser.parse
+        parsed = parse_as_file("#{hashes} #{content}")
         expect(parsed).to eq("<h#{header_level}>#{content}</h#{header_level}>")
       end
+    end
+
+    it "parses emphasis with asterisks or underscores" do
+      asterisks_content = "Hello *world*"
+      underscores_content = "Hello _world_"
+
+      parsed = parse_as_file(asterisks_content)
+      result = "Hello <em>world</em>"
+
+      expect(parsed).to eq(result)
+
+      parsed = parse_as_file(underscores_content)
+      expect(parsed).to eq(result)
+    end
+
+    it "doesn't match mixed asterisk/underscore cases" do
+      parsed = parse_as_file("hello *world_")
+      expect(parsed).to eq("hello *world_")
     end
   end
 end
